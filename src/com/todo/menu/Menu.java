a36ac76b32b0faa1de395041bce971bda1283458
윤지인 <74345861+22000487zixxin@users.noreply.github.com>
2021-09-20 17:52:03 +0900

    change theme

--- src/com/todo/menu/Menu.java
+++ src/com/todo/menu/Menu.java
@@ -4,18 +4,18 @@ public class Menu {
     public static void displaymenu()
     {
         System.out.println();
-        System.out.println("<ToDoList 관리 명령어 사용법>");
-        System.out.println("add - 항목 추가");
-        System.out.println("del - 항목 삭제");
-        System.out.println("edit - 항목 수정");
-        System.out.println("ls - 전체 목록");
-        System.out.println("ls_name_asc - 제목순 정렬");
-        System.out.println("ls_name_desc - 제목역순 정렬");
-        System.out.println("ls_date - 날짜순 정렬");
-        System.out.println("exit - 종료");
+        System.out.println("< ToDoList 관리 명령어 사용법 >");
+        System.out.println("[add] 항목 추가");
+        System.out.println("[del] 항목 삭제");
+        System.out.println("[edit] 항목 수정");
+        System.out.println("[ls] 전체 목록");
+        System.out.println("[ls_name_asc] 제목순 정렬");
+        System.out.println("[ls_name_desc] 제목역순 정렬");
+        System.out.println("[ls_date] 날짜순 정렬");
+        System.out.println("[exit] 종료");
     }
    
     public static void prompt() {
-    	System.out.println("\nCommand > ");
+    	System.out.println("\n명령어 입력 -> ");
     }
 }
